54863e6bb0d0198a3b27c27ee14837fb7b3eee5d
Sam Trenter <sntrenter@gmail.com>
2021-10-20 18:21:00 -0500

    finished reading in file + adding children/gifts, need to create nodes

--- gift.cpp
+++ gift.cpp
@@ -4,76 +4,113 @@
 #include <regex>
 using namespace std;
 
-
-
-class Child {
-    public:
-        string name;
-        int age;
-        Child(string n,int a)
+class Child
+{
+public:
+    string name;
+    int age;
+    Child(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+    void print()
+    {
+        cout << "CHILD|name:" << name << " age:" << age << endl;
+    }
+};
+class Gift
+{
+public:
+    string name;
+    int price;
+    double size;
+    bool any;
+    int ageLower;
+    int ageHigher;
+    Gift(string n, int p, double s, bool a, int al, int ah)
+    {
+        name = n;
+        price = p;
+        size = s;
+        any = a;
+        ageLower = al;
+        ageHigher = ah;
+    }
+    void print()
+    {
+        if (any)
         {
-            name = n;
-            age = a;
+            cout << "GIFT|name:" << name << " price:" << price << " size:" << size << " ageRange:"
+                 << "any" << endl; //any<< endl;//TODO: add more stuff
         }
-        void print()
+        else
         {
-            cout << "name:" << name << " age:" << age << endl;
+            cout << "GIFT|name:" << name << " price:" << price << " size:" << size << " ageRange:" << ageLower << "-" << ageHigher << endl; //TODO: add more stuff
         }
+    }
 };
-class Gift {
-    public:
-        string name;
-        int price;
-        double size;
-        bool any;
-        int ageLower;
-        int ageHigher;
-
-};
-class Node {
-    public:
-        string name = "Test";
+class Node
+{
+public:
+    string name = "Test";
 };
 
-list <Child> children = {};
+list<Child> children = {};
 list<Child>::iterator cit = children.begin();
-list <Gift> gifts = {};
+list<Gift> gifts = {};
 list<Gift>::iterator git = gifts.begin();
 
-
-void getProblem(string filename){
+void getProblem(string filename)
+{
     ifstream file;
     regex crgx("(Child[0-9]+)	age	([0-9]+)");
     regex grgx("(G[0-9]+)	([0-9]+)	([0-9]+.[0-9]+)	([0-9]+-[0-9]+|any)");
+    regex ageRangergx("([0-9]+)-([0-9]+)");
     smatch match;
+    smatch match2;
 
     cout << filename << "\n";
     filename = "examples/" + filename + ".txt";
     file.open(filename.c_str());
     string lntxt;
-    while(getline(file,lntxt)){
+    while (getline(file, lntxt))
+    {
         cout << lntxt << endl;
-        if(regex_search(lntxt,match,crgx)){
-            children.insert(cit,Child(match[1],stoi(match[2])));
+        if (regex_search(lntxt, match, crgx))
+        {
+            children.insert(cit, Child(match[1], stoi(match[2])));
         }
-        if(regex_search(lntxt,match,grgx)){
-
+        if (regex_search(lntxt, match, grgx))
+        {
+            if (match[4].str() == "any")
+            {
+                gifts.insert(git, Gift(match[1], stoi(match[2]), stod(match[3]), true, 0, 0));
+            }
+            else
+            {
+                string ageRange = match[4].str();
+                regex_search(ageRange, match2, ageRangergx);
+                gifts.insert(git, Gift(match[1], stoi(match[2]), stod(match[3]), false, stoi(match2[1]), stoi(match2[2])));
+            }
         }
     }
-    cout<<"CHILDREN:"<<endl;
-    for (auto c: children){
+    cout << "CHILDREN:" << endl;
+    for (auto c : children)
+    {
         c.print();
     }
-        
-
+    cout << "GIFTS:" << endl;
+    for (auto g : gifts)
+    {
+        g.print();
+    }
 }
 
-
-int main() {
-  cout << "Start\n";
+int main()
+{
+    cout << "Start\n";
     getProblem("ex1_3child_6gifts");
-    //Child test = Child("hello",10);
-    //test.print();
-  cout << "End\n";
-  return 0;
+    cout << "End\n";
+    return 0;
 }
